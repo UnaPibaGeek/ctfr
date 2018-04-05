a354b96681b7d0b37f3ca675a5a4472a3550dcba
Vincent Yiu <vysec@users.noreply.github.com>
2018-04-05 20:56:47 +0100

    Update ctfr.py
    Fixed bug with UTF-8

--- ctfr.py
+++ ctfr.py
@@ -2,71 +2,79 @@
 # -*- coding: utf-8 -*-
 """
 ------------------------------------------------------------------------------
-	CTFR - 04.03.18.02.10.00 - Sheila A. Berta (UnaPibaGeek)
+        CTFR - 04.03.18.02.10.00 - Sheila A. Berta (UnaPibaGeek)
 ------------------------------------------------------------------------------
 """
 
 ## # LIBRARIES # ##
+import re
 import json
 import requests
 
 ## # CONTEXT VARIABLES # ##
-version = 1.1
+version = 1.2
 
 ## # MAIN FUNCTIONS # ##
 
 def parse_args():
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-d', '--domain', type=str, required=True, help="Target domain.")
-	parser.add_argument('-o', '--output', type=str, help="Output file.")
-	return parser.parse_args()
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-d', '--domain', type=str, required=True, help="Target domain.")
+        parser.add_argument('-o', '--output', type=str, help="Output file.")
+        return parser.parse_args()
 
 def banner():
-	global version
-	from pyfiglet import figlet_format
-	b = figlet_format("        CTFR") + \
-	'''     Version {v} - Hey don't miss AXFR!
+        global version
+        b = '''
+          ____ _____ _____ ____
+         / ___|_   _|  ___|  _ \
+        | |     | | | |_  | |_) |
+        | |___  | | |  _| |  _ <
+         \____| |_| |_|   |_| \_\\
+
+     Version {v} - Hey don't miss AXFR!
     Made by Sheila A. Berta (UnaPibaGeek)
-	'''.format(v=version)
-	print(b)
+        '''.format(v=version)
+        print(b)
+
+def clear_url(target):
+        return re.sub('.*www\.','',target,1).split('/')[0].strip()
 
 def save_subdomains(subdomain,output_file):
-	with open(output_file,"a") as f:
-		f.write(subdomain + '\n')
-		f.close()
+        with open(output_file,"a") as f:
+                f.write(subdomain + '\n')
+                f.close()
 
 def main():
-	banner()
-	args = parse_args()
+        banner()
+        args = parse_args()
 
-	subdomains = []
-	target = args.domain
-	output = args.output
+        subdomains = []
+        target = clear_url(args.domain)
+        output = args.output
 
-	req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
+        req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
 
-	if req.status_code != 200:
-		print("[X] Error! Invalid domain or information not available!") 
-		exit(1)
+        if req.status_code != 200:
+                print("[X] Information not available!")
+                exit(1)
 
-	json_data = json.loads('[{}]'.format(req.text.replace('}{', '},{')))
+        json_data = json.loads('[{}]'.format(req.text.encode('utf-8','replace').replace('}{', '},{')))
 
-	for (key,value) in enumerate(json_data):
-		subdomains.append(value['name_value'])
+        for (key,value) in enumerate(json_data):
+                subdomains.append(value['name_value'])
 
-	
-	print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
 
-	subdomains = set(subdomains)
+        print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
 
-	for subdomain in subdomains:
-		print("[-]  {s}".format(s=subdomain))
-		if output is not None:
-			save_subdomains(subdomain,output)
+        subdomains = sorted(set(subdomains))
 
-	print("\n\n[!]  Done. Have a nice day! ;).")
+        for subdomain in subdomains:
+                print("[-]  {s}".format(s=subdomain))
+                if output is not None:
+                        save_subdomains(subdomain,output)
+
+        print("\n\n[!]  Done. Have a nice day! ;).")
 
 
 main()
-	
