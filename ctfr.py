3c24deb92252233558caf989f63c0b54358c2156
pl77 <pl77@users.noreply.github.com>
2019-01-11 11:21:00 -0500

    Added try/except block to 'for loop' to handle nested list exception. Also PEP8ified formatting and updated script version number.

--- ctfr.py
+++ ctfr.py
@@ -2,80 +2,89 @@
 # -*- coding: utf-8 -*-
 """
 ------------------------------------------------------------------------------
-	CTFR - 04.03.18.02.10.00 - Sheila A. Berta (UnaPibaGeek)
+    CTFR - 011.01.19.11.16.00 - Sheila A. Berta (UnaPibaGeek)
 ------------------------------------------------------------------------------
-"""
 
-## # LIBRARIES # ##
+# ## LIBRARIES ## #
+"""
 import re
 import json
 import requests
+import argparse
 
-## # CONTEXT VARIABLES # ##
-version = 1.2
+# ## CONTEXT VARIABLES ## #
+version = 1.3
 
-## # MAIN FUNCTIONS # ##
+# ## MAIN FUNCTIONS ## #
+
 
 def parse_args():
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-d', '--domain', type=str, required=True, help="Target domain.")
-	parser.add_argument('-o', '--output', type=str, help="Output file.")
-	return parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--domain', type=str, required=True, help="Target domain.")
+    parser.add_argument('-o', '--output', type=str, help="Output file.")
+    return parser.parse_args()
+
 
 def banner():
-	global version
-	b = '''
+    global version
+    b = '''
           ____ _____ _____ ____  
          / ___|_   _|  ___|  _ \ 
         | |     | | | |_  | |_) |
         | |___  | | |  _| |  _ < 
          \____| |_| |_|   |_| \_\\
-	
+    
      Version {v} - Hey don't miss AXFR!
     Made by Sheila A. Berta (UnaPibaGeek)
-	'''.format(v=version)
-	print(b)
-	
-def clear_url(target):
-	return re.sub('.*www\.','',target,1).split('/')[0].strip()
+    '''.format(v=version)
+    print(b)
 
-def save_subdomains(subdomain,output_file):
-	with open(output_file,"a") as f:
-		f.write(subdomain + '\n')
-		f.close()
+
+def clear_url(target):
+    return re.sub('.*www\.', '', target, 1).split('/')[0].strip()
+
+
+def save_subdomains(subdomain, output_file):
+    with open(output_file, "a") as f:
+        f.write(subdomain + '\n')
+        f.close()
+
 
 def main():
-	banner()
-	args = parse_args()
+    banner()
+    args = parse_args()
 
-	subdomains = []
-	target = clear_url(args.domain)
-	output = args.output
+    subdomains = []
+    target = clear_url(args.domain)
+    output = args.output
 
-	req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
+    req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
 
-	if req.status_code != 200:
-		print("[X] Information not available!") 
-		exit(1)
+    if req.status_code != 200:
+        print("[X] Information not available!")
+        exit(1)
 
-	json_data = json.loads('[{}]'.format(req.text.replace('}{', '},{')))
+    json_data = json.loads('[{}]'.format(req.text.replace('}{', '},{')))
 
-	for (key,value) in enumerate(json_data):
-		subdomains.append(value['name_value'])
+    # json.loads seems to now return a list, which resulted in nesting and broke the 'for' loop.
+    # rewrote with a try/except loop to maintain compatibility with previous versions.
+    try:
+        for (key, value) in enumerate(json_data):
+            subdomains.append(value['name_value'])
+    except TypeError:
+        json_data = json.loads('{}'.format(req.text.replace('}{', '},{')))
+        for (key, value) in enumerate(json_data):
+            subdomains.append(value['name_value'])
+    print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
 
-	
-	print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
+    subdomains = sorted(set(subdomains))
 
-	subdomains = sorted(set(subdomains))
+    for subdomain in subdomains:
+        print("[-]  {s}".format(s=subdomain))
+        if output is not None:
+            save_subdomains(subdomain, output)
 
-	for subdomain in subdomains:
-		print("[-]  {s}".format(s=subdomain))
-		if output is not None:
-			save_subdomains(subdomain,output)
-
-	print("\n\n[!]  Done. Have a nice day! ;).")
+    print("\n\n[!]  Done. Have a nice day! ;).")
 
 
 main()
-	
