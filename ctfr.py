fa1b5c520d15b9794a0904678eea6436bd6ef6f7
Mariano Verdú <vdg0@protonmail.com>
2019-03-02 14:43:20 -0300

    Elude open the same file multiple times.

--- ctfr.py
+++ ctfr.py
@@ -11,7 +11,7 @@ import re
 import requests
 
 ## # CONTEXT VARIABLES # ##
-version = 1.2
+version = 1.3
 
 ## # MAIN FUNCTIONS # ##
 
@@ -39,10 +39,6 @@ def banner():
 def clear_url(target):
 	return re.sub('.*www\.','',target,1).split('/')[0].strip()
 
-def save_subdomains(subdomain,output_file):
-	with open(output_file,"a") as f:
-		f.write(subdomain + '\n')
-		f.close()
 
 def main():
 	banner()
@@ -66,12 +62,16 @@ def main():
 
 	subdomains = sorted(set(subdomains))
 
-	for subdomain in subdomains:
-		print("[-]  {s}".format(s=subdomain))
-		if output is not None:
-			save_subdomains(subdomain,output)
+	if output is not None:
+		with open(output,"a") as f:
+			for subdomain in subdomains:
+				print("[-]  {s}".format(s=subdomain))
+				f.write(subdomain + '\n')
+	else:
+		for subdomain in subdomains:
+			print("[-]  {s}".format(s=subdomain))
 
-	print("\n\n[!]  Done. Have a nice day! ;).")
+	print("\n\n[!]  Done. May the Force be with you :3")
 
 
 main()
