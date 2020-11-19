4a131d8a00a635c37c6806b842c632b4089ddd5f
ali <marefati110@gmail.com>
2020-11-19 15:15:41 +0330

    add __name__ condition

--- ctfr.py
+++ ctfr.py
@@ -15,16 +15,19 @@ version = 1.2
 
 ## # MAIN FUNCTIONS # ##
 
+
 def parse_args():
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-d', '--domain', type=str, required=True, help="Target domain.")
-	parser.add_argument('-o', '--output', type=str, help="Output file.")
-	return parser.parse_args()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--domain', type=str,
+                        required=True, help="Target domain.")
+    parser.add_argument('-o', '--output', type=str, help="Output file.")
+    return parser.parse_args()
+
 
 def banner():
-	global version
-	b = '''
+    global version
+    b = '''
           ____ _____ _____ ____  
          / ___|_   _|  ___|  _ \ 
         | |     | | | |_  | |_) |
@@ -34,45 +37,47 @@ def banner():
      Version {v} - Hey don't miss AXFR!
     Made by Sheila A. Berta (UnaPibaGeek)
 	'''.format(v=version)
-	print(b)
-	
-def clear_url(target):
-	return re.sub('.*www\.','',target,1).split('/')[0].strip()
+    print(b)
 
-def save_subdomains(subdomain,output_file):
-	with open(output_file,"a") as f:
-		f.write(subdomain + '\n')
-		f.close()
+
+def clear_url(target):
+    return re.sub('.*www\.', '', target, 1).split('/')[0].strip()
+
+
+def save_subdomains(subdomain, output_file):
+    with open(output_file, "a") as f:
+        f.write(subdomain + '\n')
+        f.close()
+
 
 def main():
-	banner()
-	args = parse_args()
+    banner()
+    args = parse_args()
 
-	subdomains = []
-	target = clear_url(args.domain)
-	output = args.output
+    subdomains = []
+    target = clear_url(args.domain)
+    output = args.output
 
-	req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
+    req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
 
-	if req.status_code != 200:
-		print("[X] Information not available!") 
-		exit(1)
+    if req.status_code != 200:
+        print("[X] Information not available!")
+        exit(1)
 
-	for (key,value) in enumerate(req.json()):
-		subdomains.append(value['name_value'])
+    for (key, value) in enumerate(req.json()):
+        subdomains.append(value['name_value'])
 
-	
-	print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
+    print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
 
-	subdomains = sorted(set(subdomains))
+    subdomains = sorted(set(subdomains))
 
-	for subdomain in subdomains:
-		print("[-]  {s}".format(s=subdomain))
-		if output is not None:
-			save_subdomains(subdomain,output)
+    for subdomain in subdomains:
+        print("[-]  {s}".format(s=subdomain))
+        if output is not None:
+            save_subdomains(subdomain, output)
 
-	print("\n\n[!]  Done. Have a nice day! ;).")
+    print("\n\n[!]  Done. Have a nice day! ;).")
 
 
-main()
-	
+if __name__ == "__main__":
+	main()
