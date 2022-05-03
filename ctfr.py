cc8093f64f726cdaa3a38c20d29b6420c2870261
h4r1337 <ghostsquad139@gmail.com>
2022-05-03 18:21:09 +0530

    added new features

--- ctfr.py
+++ ctfr.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 """
 ------------------------------------------------------------------------------
-	CTFR - 04.03.18.02.10.00 - Sheila A. Berta (UnaPibaGeek)
+    CTFR - 04.03.18.02.10.00 - Sheila A. Berta (UnaPibaGeek)
 ------------------------------------------------------------------------------
 """
 
 ## # LIBRARIES # ##
 import re
 import requests
+import sys
 
 ## # CONTEXT VARIABLES # ##
 version = 1.2
@@ -16,63 +17,77 @@ version = 1.2
 ## # MAIN FUNCTIONS # ##
 
 def parse_args():
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-d', '--domain', type=str, required=True, help="Target domain.")
-	parser.add_argument('-o', '--output', type=str, help="Output file.")
-	return parser.parse_args()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--domain', type=str, help="Target domain.")
+    parser.add_argument('-o', '--output', type=str, help="Output file.")
+    parser.add_argument('-s', '--silent', default=False, action='store_true', help="Don't show banner")
+    return parser.parse_args()
 
 def banner():
-	global version
-	b = '''
+    global version
+    b = '''
           ____ _____ _____ ____  
          / ___|_   _|  ___|  _ \ 
         | |     | | | |_  | |_) |
         | |___  | | |  _| |  _ < 
          \____| |_| |_|   |_| \_\\
-	
+    
      Version {v} - Hey don't miss AXFR!
     Made by Sheila A. Berta (UnaPibaGeek)
-	'''.format(v=version)
-	print(b)
-	
+    '''.format(v=version)
+    print(b)
+    
 def clear_url(target):
-	return re.sub('.*www\.','',target,1).split('/')[0].strip()
+    return re.sub('.*www\.','',target,1).split('/')[0].strip()
 
 def save_subdomains(subdomain,output_file):
-	with open(output_file,"a") as f:
-		f.write(subdomain + '\n')
-		f.close()
+    with open(output_file,"a") as f:
+        f.write(subdomain + '\n')
+        f.close()
+
+def get_subdomain(target, subdomains, silent):
+    req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
+
+    if req.status_code != 200:
+        if not silent: print("[X] Information not available!") 
+        return
+
+    for (key,value) in enumerate(req.json()):
+        subdomains.append(value['name_value'])
+
+    if not silent: print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
+
+    subdomains = sorted(set(subdomains))
+    return subdomains
 
 def main():
-	banner()
-	args = parse_args()
+    args = parse_args()
+    silent = args.silent
+    if not silent: banner()
+    subdomains = []
 
-	subdomains = []
-	target = clear_url(args.domain)
-	output = args.output
+    stdin = not sys.stdin.isatty()
+    output = args.output
+    if args.domain: 
+        target = clear_url(args.domain)
+        subdomains = get_subdomain(target, subdomains, silent)
+    elif stdin:
+        for line in sys.stdin:
+            target = clear_url(line.rstrip())
+            subdomains = get_subdomain(target, subdomains, silent)
+    else:
+        print("No domain given. either use stdin or -d flag")
+        exit(1)
 
-	req = requests.get("https://crt.sh/?q=%.{d}&output=json".format(d=target))
+    for subdomain in subdomains:
+        print("{s}".format(s=subdomain))
+        if output is not None:
+            save_subdomains(subdomain,output)
 
-	if req.status_code != 200:
-		print("[X] Information not available!") 
-		exit(1)
-
-	for (key,value) in enumerate(req.json()):
-		subdomains.append(value['name_value'])
-
-	
-	print("\n[!] ---- TARGET: {d} ---- [!] \n".format(d=target))
-
-	subdomains = sorted(set(subdomains))
-
-	for subdomain in subdomains:
-		print("[-]  {s}".format(s=subdomain))
-		if output is not None:
-			save_subdomains(subdomain,output)
-
-	print("\n\n[!]  Done. Have a nice day! ;).")
+    if not silent: print("\n\n[!]  Done. Have a nice day! ;).")
 
 
-main()
-	
+if __name__ == '__main__':
+    main()
+    
